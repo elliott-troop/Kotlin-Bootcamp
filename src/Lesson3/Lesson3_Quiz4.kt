e3d18619723b070d1077375cd55275a68dd7aed0
Elliott Porter <elliottp731@gmail.com>
2020-03-07 17:18:03 -0500

    Udacity Kotlin Bootcamp Lesson 3 - Moved into separate folder

--- src/Lesson3/Lesson3_Quiz4.kt
+++ src/Lesson3/Lesson3_Quiz4.kt
@@ -1,3 +1,5 @@
+package Lesson3
+
 /**
  * Udacity - Kotlin Bootcamp for Programmers
  * Lesson 3: Functions
@@ -5,9 +7,9 @@
  */
 
 fun main(args: Array<String>) {
-    println(canAddFish(10.0, listOf(3,3,3)))
-    println(canAddFish(8.0, listOf(2,2,2), hasDecorations = false))
-    println(canAddFish(9.0, listOf(1,1,3), 3))
+    println(canAddFish(10.0, listOf(3, 3, 3)))
+    println(canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1, 1, 3), 3))
     println(canAddFish(10.0, listOf(), 7, true))
 }
 
